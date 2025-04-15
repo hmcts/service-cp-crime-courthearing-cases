688172b19631b3c76e368245bee5310b303ff26d
Munish Jalota <mjalota@gmail.com>
2025-04-15 10:32:36 +0100

    api impl (#3)

--- src/main/java/uk/gov/hmcts/cp/controllers/OpenApiController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/OpenApiController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.cp.openapi.api.DefaultApi;
-import uk.gov.hmcts.cp.openapi.model.CourtHousesschema;
+import uk.gov.hmcts.cp.openapi.model.CaseJudiciaryResult;
 import uk.gov.hmcts.cp.services.OpenApiService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +17,9 @@ public class OpenApiController implements DefaultApi {
     private final OpenApiService openApiService;
 
     @Override
-    public ResponseEntity<CourtHousesschema> courthousesCourtIdGet(String courtId) {
-        CourtHousesschema courtHousesschema = openApiService.getCourtHouse(courtId);
-        return new ResponseEntity<>(courtHousesschema, HttpStatus.OK);
+    public ResponseEntity<List<CaseJudiciaryResult>> getCaseLevelResults(String caseId) {
+        List<CaseJudiciaryResult> caseLevelResultsList = openApiService.getCaseLevelResults(caseId);
+        return new ResponseEntity<>(caseLevelResultsList, HttpStatus.OK);
     }
 
 }
