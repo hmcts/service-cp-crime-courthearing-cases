d0fc3266e77d6583518744ea6d2e7599ff96ca46
Munish Jalota <Munish.Jalota@justice.gov.uk>
2025-06-03 11:35:43 +0100

    updated code as per the ruleset

--- src/main/java/uk/gov/hmcts/cp/repositories/CourtHearingCasesRepository.java
+++ src/main/java/uk/gov/hmcts/cp/repositories/CourtHearingCasesRepository.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.cp.openapi.model.CaseJudiciaryResponse;
 
 @Repository
+@FunctionalInterface
 public interface CourtHearingCasesRepository {
 
     CaseJudiciaryResponse getCaseLevelResults(String caseId);
