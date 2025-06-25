cbf4552c52bda9bf0399e262f6d1f775750805a6
Vipin Palengara <Vipin.Palengara@justice.gov.uk>
2025-06-25 12:53:12 +0100

    adding pact verifier test

--- src/main/java/uk/gov/hmcts/cp/repositories/CourtHearingCasesRepository.java
+++ src/main/java/uk/gov/hmcts/cp/repositories/CourtHearingCasesRepository.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.cp.openapi.model.CaseJudiciaryResponse;
 
 @Repository
-@FunctionalInterface
 public interface CourtHearingCasesRepository {
 
     CaseJudiciaryResponse getCaseLevelResults(String caseId);
-
+    void saveCaseLevelResults(String caseUrn, CaseJudiciaryResponse courtHouseResponse);
+    void clearAll();
 }
